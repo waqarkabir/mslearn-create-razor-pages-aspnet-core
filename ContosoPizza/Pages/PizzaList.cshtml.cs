b62d8a40ded01e55c2f833bcec81d2512a053dbb
waqarkabir <waqarkabir10@gmail.com>
2024-11-12 15:17:31 +0500

    Completed CRUD for Pizza App

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -8,6 +8,8 @@ namespace ContosoPizza.Pages
     public class PizzaListModel : PageModel
     {
         private readonly PizzaService _service;
+        [BindProperty]
+        public Pizza NewPizza {get; set;} = default!;
         public IList<Pizza> PizzaList {get; set;} = default!;
 
         public PizzaListModel(PizzaService service)
@@ -17,6 +19,25 @@ namespace ContosoPizza.Pages
         public void OnGet()
         {
             PizzaList = _service.GetPizzas();
+        }
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+
+            _service.AddPizza(NewPizza);
+
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            _service.DeletePizza(id);
+
+            return RedirectToAction("Get");
         }
     }
 }
